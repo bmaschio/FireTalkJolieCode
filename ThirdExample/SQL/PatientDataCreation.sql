7aac38d806124c2ffc948e5c1c5cfe3994590a21
Balint Maschio <bmaschio77@gmail.com>
2016-12-23 14:15:10 +0100

    SQL script creation DB postgres

---
+++ ThirdExample/SQL/PatientDataCreation.sql
@@ -0,0 +1,28 @@
+﻿-- Database: "PatientData"
+
+-- DROP DATABASE "PatientData";
+
+CREATE DATABASE "PatientData"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Italian_Italy.1252'
+       LC_CTYPE = 'Italian_Italy.1252'
+       CONNECTION LIMIT = -1;
+
+ CREATE TABLE public.patient_image
+(
+  patient_id character varying,
+  image_name character varying,
+  image_type character varying,
+  exam_id character varying,
+  image_ypos double precision,
+  image_xpos double precision
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.patient_image
+  OWNER TO postgres;
+
+
